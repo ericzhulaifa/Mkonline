fb68bce3e4ea393d8164cefeee724a06dfab243f
ericzhulaifa <zhulaifa@hotmail.com>
2020-02-08 15:08:35 +0800

    compete org-detail-homepage.html page.

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -4,11 +4,13 @@ __date__ = '2020/2/5 7:32 上午'
 
 from django.urls import path
 
-from .views import OrgView, AddAskView
+from .views import OrgView, AddAskView, OrgHomeView
 
 
 app_name = 'organization'
 urlpatterns = [
     path('list/', OrgView.as_view(), name='list'),
     path('add_ask/', AddAskView.as_view(), name='add_ask'),
+
+    path('<int:org_id>/', OrgHomeView.as_view(), name='org_home')
 ]
--- apps/organization/views.py
+++ apps/organization/views.py
@@ -11,6 +11,22 @@ from apps.organization.forms import AddAskForm
 # Create your views here.
 
 
+class OrgHomeView(View):
+    def get(self, request, org_id, *args, **kwargs):
+        course_org = CourseOrg.objects.get(id=int(org_id))
+        course_org.click_nums += 1
+        course_org.save()
+
+        all_courses = course_org.course_set.all()[:3]
+        all_teachers = course_org.teacher_set.all()[:1]
+
+        return render(request, "org-detail-homepage.html", {
+            "all_courses": all_courses,
+            "all_teachers": all_teachers,
+            "course_org": course_org,
+        })
+
+
 class AddAskView(View):
     """
     处理用户咨询
